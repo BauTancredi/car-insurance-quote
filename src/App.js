8a05ebdea110269140b38fb104b9ea9438bde7e0
BauTancredi <bautistatancredi@hotmail.com>
2020-04-12 16:23:01 -0300

    Form component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import styled from "@emotion/styled";
 import Header from "./components/Header";
+import Form from "./components/Form";
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const FormContainer = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
+
 function App() {
-  return <Header title="Car insurance quote" />;
+  return (
+    <Container>
+      <Header title="Car insurance quote" />;
+      <FormContainer>
+        <Form />
+      </FormContainer>
+    </Container>
+  );
 }
 
 export default App;
