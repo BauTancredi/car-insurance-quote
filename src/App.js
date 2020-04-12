a8d1c666bd50a5fbfea880e36830ebeed47a50ea
BauTancredi <bautistatancredi@hotmail.com>
2020-04-12 16:12:32 -0300

    Header component and styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
-
+import Header from "./components/Header";
 function App() {
-  return (
-    <div className="App">
-      <h1>Hola</h1>
-    </div>
-  );
+  return <Header title="Car insurance quote" />;
 }
 
 export default App;
